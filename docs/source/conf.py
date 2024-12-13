bc5046d83062ca3374905957cd88f2995b6bfe64
Eliot-P <eliot.peeters@student.uclouvain>
2024-12-13 11:40:17 +0100

    second test commit

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'OpenTPS'
+copyright = '2024, OpenTPS team'
+author = 'OpenTPS team'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx_gallery.gen_gallery']
+
+sphinx_gallery_conf = {
+     'examples_dirs': '../../examples',   # path to your example scripts
+     'gallery_dirs': 'auto_examples',  # path to where to save gallery generated output
+}
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+html_theme_options = { "launch_buttons":{"colab_url": "https://colab.research.google.com"},
+                       "repository_url": "https://github.com/OpenTPS/examples",
+                       "path_to_docs": "docs/source",
+                       "repository_branch": "master",
+                       "use_edit_page_button": True,
+                       "use_issues_button": True,
+                       "use_repository_button": True,
+                       "use_download_button": True,
+                       }
+
