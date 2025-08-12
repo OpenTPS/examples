c112a4686594f1e1e94d7989cc445e1a0a8935fc
Romane Carryn <romane.carryn@student.uclouvain.be>
2025-08-12 15:31:06 +0200

    other example

---
+++ examples/OthersExample/create3DSeqFromDicom.py
@@ -0,0 +1,55 @@
+'''
+Create 3D Sequence from DICOM
+=========================
+author: OpenTPS team
+
+This example shows how to read data from a 4DCT folder, create a dynamic 3D sequence with the 4DCT data, and save this sequence in serialized format on the drive.
+'''
+# %% [skip]
+# installing opentps in colab
+import sys
+if "google.colab" in sys.modules:
+    from IPython import get_ipython
+    get_ipython().system('git clone https://gitlab.com/openmcsquare/opentps.git')
+    get_ipython().system('pip install ./opentps')
+    import opentps
+
+#%%
+#imports
+import os
+from pathlib import Path
+import sys
+
+#%%
+#import the needed opentps.core packages
+
+from opentps.core.io.dataLoader import readData
+from opentps.core.data.dynamicData._dynamic3DSequence import Dynamic3DSequence
+from opentps.core.io.serializedObjectIO import saveSerializedObjects
+
+#%%
+# Get the current working directory, its parent, then add the testData folder at the end of it
+testDataPath = os.path.join(Path(os.getcwd()).parent.absolute(), 'opentps/testData/')
+
+#%%
+## read a serialized dynamic sequence
+dataPath = testDataPath + "4DCTDicomLight"
+
+print('Datas present in ' + dataPath + 'are loaded.')
+dataList = readData(dataPath)
+print(len(dataList), 'images found in the folder')
+print('Image type =', type(dataList[0]))
+
+#%%
+# create a Dynamic3DSequence and change its name
+dynseq = Dynamic3DSequence(dyn3DImageList=dataList)
+print('Type of the created object =', type(dynseq))
+print('Sequence name =', dynseq.name)
+dynseq.name = 'Light4DCT'
+print('Sequence name = ', dynseq.name)
+print('Sequence lenght =', len(dynseq.dyn3DImageList))
+
+#%%
+# save it as a serialized object
+savingPath = testDataPath + 'lightDynSeq'
+saveSerializedObjects(dynseq, savingPath)
---
+++ examples/OthersExample/crop3DDataAroundROI.py
@@ -0,0 +1,58 @@
+'''
+Crop 3D Data Around ROI
+=========================
+author: OpenTPS team
+
+This example shows how to: 
+- Read a serialized patient with a Dynamic3DSequence, a Dynamic3DModel and an RTStruct
+!! The data is not given in the test data folder of the project !!
+- Select an ROI from the RTStruct object
+- Get the ROI as an ROIMask
+- Get the box around the ROI in scanner coordinates
+- Crop the dynamic sequence and the dynamic model around the box
+'''
+# %% [skip]
+# installing opentps in colab
+import sys
+if "google.colab" in sys.modules:
+    from IPython import get_ipython
+    get_ipython().system('git clone https://gitlab.com/openmcsquare/opentps.git')
+    get_ipython().system('pip install ./opentps')
+    import opentps
+
+#%%
+#imports
+import os
+import sys
+
+#%%
+#import the needed opentps.core packages
+from opentps.core.processing.imageProcessing.resampler3D import crop3DDataAroundBox
+from opentps.core.processing.segmentation.segmentation3D import getBoxAroundROI
+from opentps.core.io.serializedObjectIO import loadDataStructure
+
+#%%
+# Load the serialized patient data
+dataPath = '/data/Patient0BaseAndMod.p'
+patient = loadDataStructure(dataPath)[0]
+
+dynSeq = patient.getPatientDataOfType("Dynamic3DSequence")[0]
+dynMod = patient.getPatientDataOfType("Dynamic3DModel")[0]
+rtStruct = patient.getPatientDataOfType("RTStruct")[0]
+
+#%%
+# get the ROI and mask on which we want to apply the motion signal
+print('Available ROIs')
+rtStruct.print_ROINames()
+bodyContour = rtStruct.getContourByName('body')
+ROIMask = bodyContour.getBinaryMask(origin=dynMod.midp.origin, gridSize=dynMod.midp.gridSize, spacing=dynMod.midp.spacing)
+
+#%%
+# get the box around the ROI
+box = getBoxAroundROI(ROIMask)
+marginInMM = [10, 10, 10]
+#%%
+# crop the dynamic sequence and the dynamic model around the box
+crop3DDataAroundBox(dynSeq, box, marginInMM=marginInMM)
+print('-'*50)
+crop3DDataAroundBox(dynMod, box, marginInMM=marginInMM) 
