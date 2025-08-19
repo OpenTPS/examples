aeec189d7af3d4859f4f9e34eb80dea59e639d8a
Romane Carryn <romane.carryn@student.uclouvain.be>
2025-08-19 13:00:45 +0200

    fix bugs

--- examples/multiplePythonEnv/lol.py
+++
@@ -1,9 +0,0 @@
-from multiprocessing import shared_memory
-
-try:
-    shm = shared_memory.SharedMemory(name='sharedArray')
-    shm.close()
-    shm.unlink()
-    print("Shared memory 'sharedArray' successfully removed.")
-except FileNotFoundError:
-    print("No shared memory named 'sharedArray' found.")
